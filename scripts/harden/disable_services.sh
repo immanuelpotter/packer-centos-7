7bb6858cd4bc63fa8fcf0fe8f106ad03a9e8707a
Immanuel Potter <immanuel.potter@homeoffice.gsi.gov.uk>
2018-08-08 11:47:36 +0100

    added harden module

--- scripts/harden/disable_services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-disable_service(){
-  service_to_disable="$1"
-  chkconfig $service_to_disable off && echo "Service $service_to_disable disabled" || echo "Service $service_to_disable does not exist. Good!" 
-}
--- scripts/harden/ensure_gpg_configured.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ensure_gpg(){
-  echo "Checking gpg-pubkey is installed..."
-  gpg_results="$(rpm -q gpg-pubkey --qf '%{name}-%{version}-%{release} --> %{summary}\n')"
-  echo -e "GPG enforcement status: ${gpg_results}\nMake sure the package manager GPG keys are in accordance with site policy."
-}
